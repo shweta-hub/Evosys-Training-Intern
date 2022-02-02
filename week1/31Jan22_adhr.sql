9aac5db8e488a71069cca0786b45d5ec8dd34a2e
FinalYearProjectVCET <62585803+shweta-hub@users.noreply.github.com>
2022-02-02 17:45:28 +0530

    .

--- week1/31Jan22_adhr.sql
+++ week1/31Jan22_adhr.sql
@@ -563,3 +563,158 @@ SELECT
     ORDER BY
     deptno;
     
+===============================================================
+SELECT * FROM BRANCH;
+SELECT * FROM DEPT;
+SELECT * FROM EMP;
+
+ALTER TABLE emp
+DROP COLUMN branchno;
+-- ################################################################ 
+-- JOIN 
+-- THERE ARE TWO WAYS TO FETCH DATA FROM ORACLE DATABASE 
+-- EITHER USING (+) OR USING ANSI LEFT/OUTER/INNER/RIGHT QURIES
+-- Oracle syntax dont have full outer join but Ansi version has it
+-- ################################################################ 
+
+
+-- ################################################################
+-- INNER JOIN
+-- 18.0 Display employee details such that ename,deptname and location of work is printed by the query
+-- ################################################################
+SELECT
+    emp.ename,dept.dname,branch.location
+FROM 
+    emp,dept,branch
+where 
+    emp.deptno=dept.deptno and dept.branchno=branch.branchno;
+
+SELECT
+    emp.ename,dept.dname,branch.location
+FROM 
+    emp JOIN dept 
+    ON emp.deptno=dept.deptno
+    JOIN branch
+    ON dept.branchno=branch.branchno;
+
+-- ################################################################ 
+-- LIST All employees details suhc as  EMPNAME, JOB, MGR, HIGREDATE, SAL,DNAME
+-- left join to dept table 
+-- All employee details along with dname even if some employee has null value in deptno.
+--    EMP [ EMPNAME, JOB, MGR, HIGREDATE, SAL]
+--    DEPT[ DNAME]
+--   EMP.DEPTNO=DEPT.DEPTNO
+-- ################################################################ 
+SELECT 
+    ENAME,JOB,MGR,HIREDATE,SAL,DNAME
+FROM
+EMP LEFT OUTER JOIN DEPT
+ON
+EMP.DEPTNO=DEPT.DEPTNO;
+-- ################################################################ 
+-- Display ename,job,mgr,sal,hiredate,dname from emp and dept 
+-- such that all deptartment names are displayed in ouput
+-- Display ename,job,mgr,sal,hiredate,dname from emp showing right join with dept 
+ ################################################################ 
+SELECT 
+    ENAME,JOB,MGR,HIREDATE,SAL,DNAME
+FROM
+EMP RIGHT OUTER JOIN DEPT
+ON
+EMP.DEPTNO=DEPT.DEPTNO;
+
+-- ################################################################ 
+-- Display ename,job,mgr,sal,hiredate,dname from emp and dept 
+-- EVEN FOR THE NULL VALUES IN BOTH TABLES
+ -- ################################################################ 
+ SELECT 
+    ENAME,JOB,MGR,HIREDATE,SAL,DNAME
+FROM
+    EMP FULL OUTER JOIN DEPT
+ON
+    EMP.DEPTNO=DEPT.DEPTNO
+ORDER BY DNAME ;
+
+--
+ SELECT 
+    ENAME,JOB,MGR,HIREDATE,SAL,e.deptno,DNAME
+FROM
+    EMP e JOIN DEPT d
+ON
+    e.DEPTNO=d.DEPTNO
+ORDER BY DNAME ;
+
+
+SELECT ENAME, E.job, MGR,HIREDATE,SAL,D.DNAME
+FROM EMP E , DEPT D
+WHERE E.DEPTNO=D.DEPTNO(+)
+
+SELECT ENAME, E.job, MGR,HIREDATE,SAL,D.DNAME
+FROM EMP E , DEPT D
+WHERE E.DEPTNO(+)=D.DEPTNO
+
+
+-- ################################################################ 
+-- list ename,job,sal,branchname
+-- emp
+-- branch
+-- ################################################################ 
+SELECT 
+    ENAME,JOB,SAL,BRANCHNAME
+FROM
+        EMP e join Dept d
+    on 
+        e.deptno=d.deptno
+    join
+        branch b
+    on
+        d.branchno=b.branchno;
+
+-- ################################################################ 
+-- list empname, job,sal,branchname,dname
+-- ################################################################ 
+--aSSIGNMENT
+-- ################################################################ 
+-- LIST ALL DNAME AND EMP COUNT FOR EACH DEPT 
+-- ################################################################ 
+SELECT
+    DNAME,COUNT(EMPNO)
+FROM
+    DEPT JOIN EMP
+ON
+    DEPT.DEPTNO=EMP.DEPTNO
+GROUP BY 
+    DNAME;
+    
+    
+
+-- ################################################################ 
+-- LIST ALL BRANCHNAME AND EMP COUNT FOR EACH BRANCH
+-- ################################################################ 
+SELECT 
+    BRANCHNAME,COUNT(EMPNO)
+FROM
+    BRANCH JOIN DEPT
+    ON BRANCH.BRANCHNO=DEPT.BRANCHNO
+    JOIN EMP
+    ON DEPT.DEPTNO=EMP.DEPTNO
+GROUP BY
+    branchname;
+-- ################################################################ 
+--LIST BRANCHNAME,DNAME,SUM OF SAL FOR THOSE BRANCH AND DEPT WHERE THE SUM IS >5000
+-- ################################################################ 
+
+
+-- ################################################################ 
+-- List empno,ename,mgr,manger name 
+-- ################################################################ 
+SELECT
+    e.empno,e.ename,e.mgr,m.ename
+FROM 
+    emp e  join emp m
+    on e.mgr=m.empno;
+
+
+
+
+    
